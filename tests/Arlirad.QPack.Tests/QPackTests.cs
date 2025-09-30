33eaddcf1a2cbd8351fcf1667476bb6f6733c6a9
Arlirad <arlirad@nodivia.pl>
2025-09-30 20:45:46 +0200

    Add stream ID tracking and RFC 9204 B.1. example test

--- tests/Arlirad.QPack.Tests/QPackTests.cs
+++ tests/Arlirad.QPack.Tests/QPackTests.cs
@@ -1,8 +1,11 @@
-﻿using Arlirad.QPack.Streams;
+﻿using Arbiter.DTOs;
+using Arlirad.QPack.Decoding;
+using Arlirad.QPack.Streams;
+using Arlirad.QPack.Tests.Helpers;
 
 namespace Arlirad.QPack.Tests;
 
-public class Tests
+public class QPackTests
 {
     private readonly Dictionary<int, byte[]> _integers = new()
     {
@@ -27,5 +30,38 @@ public class Tests
 
         var stream42 = new QPackStream(new MemoryStream(_integers[42]));
         Assert.That(stream42.ReadVarInt(8), Is.EqualTo(42));
+    }
+
+    /// <summary>
+    /// B.1. Literal Field Line with Name Reference
+    /// The encoder sends an encoded field section containing a literal representation of a field with a static name
+    /// reference.
+    /// </summary>
+    [Test]
+    public void QPackLiteralFieldLineWithNameReferenceTest()
+    {
+        var reader = new QPackDecoder();
+        const string example = """
+                               Stream: 0
+                               0000                | Required Insert Count = 0, Base = 0
+                               510b 2f69 6e64 6578 | Literal Field Line with Name Reference
+                               2e68 746d 6c        |  Static Table, Index=1
+                                                   |  (:path=/index.html)
+
+                                                             Abs Ref Name        Value
+                                                             ^-- acknowledged --^
+                                                             Size=0
+                               """;
+
+        var streams = RFCHelper.GetRfcExampleBytes(example);
+        var stream0Section = reader.GetSectionReader(streamId: 0, streams[0]).GetAwaiter().GetResult();
+        var stream0Headers = new HttpHeaders();
+
+        foreach (var field in stream0Section)
+        {
+            stream0Headers[field.Name] = field.Value;
+        }
+
+        Assert.That(stream0Headers[":path"], Is.EqualTo("/index.html"));
     }
 }
