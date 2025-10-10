904894e0d5fb2be86ef16f9bf3e5ae986f6db4db
UTFTymoteusz <arlirad@nodivia.pl>
2025-10-10 21:58:46 +0200

    Fix TransactionHandler not sending 404
    Add SendResponse call when setting NotFound when site is not found.

--- src/Arbiter.Application/Handlers/TransactionHandler.cs
+++ src/Arbiter.Application/Handlers/TransactionHandler.cs
@@ -1,6 +1,8 @@
+using Arbiter.Application.DTOs;
 using Arbiter.Application.Interfaces;
 using Arbiter.Application.Managers;
 using Arbiter.Application.Mappers;
+using Arbiter.Domain.Aggregates;
 using Arbiter.Domain.Enums;
 
 namespace Arbiter.Application.Handlers;
@@ -23,11 +25,17 @@ internal class TransactionHandler(
         if (site is null)
         {
             await context.Response.Set(Status.NotFound);
+            await SendResponse(transaction, context);
+
             return;
         }
 
         await site.HandleDelegate(context);
+        await SendResponse(transaction, context);
+    }
 
+    private async Task SendResponse(ITransaction transaction, Context context)
+    {
         var response = contextMapper.ToDto(context);
 
         await transaction.SetResponse(response);
