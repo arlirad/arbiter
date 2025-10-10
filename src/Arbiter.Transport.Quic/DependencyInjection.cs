14731d66d6fd72b028a322df83c70ab084f049df
Arlirad <arlirad@nodivia.pl>
2025-10-11 01:27:20 +0200

    Introduce global middlewares
    Add IGlobalMiddleware to facilitate application-level middlewares.
    
    Support singleton global middlewares via GlobalMiddlewareInjection.
     - Insert Alt-Svc headers to allow upgrading to QUIC connections with
    QuicAltSvcGlobalMiddleware
     - Return 404 when a site is not found with NullSiteGlobalMiddleware
    
    Move SiteManagerConfigurator and QuicAcceptorConfigurator to
    respective Configuration directories.

--- src/Arbiter.Transport.Quic/DependencyInjection.cs
+++ src/Arbiter.Transport.Quic/DependencyInjection.cs
@@ -1,4 +1,6 @@
 using Arbiter.Application.Interfaces;
+using Arbiter.Application.Middleware;
+using Arbiter.Transport.Quic.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Arbiter.Transport.Quic;
@@ -8,6 +10,12 @@ public static class DependencyInjection
     public static void AddQuicTransport(this IServiceCollection services)
     {
         services.AddSingleton<IConfigurator, QuicAcceptorConfigurator>();
+        services.AddSingleton<IConfigurator, QuicAltSvcGlobalMiddlewareConfigurator>();
         services.AddSingleton<IAcceptor, QuicAcceptor>();
+    }
+
+    public static void AddQuicGlobalMiddleware(this IServiceCollection services)
+    {
+        services.AddGlobalMiddleware<QuicAltSvcGlobalMiddleware>();
     }
 }
