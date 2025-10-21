fe3ccdc239b0e77cbdd09765e9cbc1754e69ce06
Arlirad <arlirad@nodivia.pl>
2025-10-21 21:21:19 +0200

    Introduce Arlirad.Mediator
    Add reflection-based Mediator implementation with a method that flushes
    cached types to facilitate future dynamic class reloading.

---
+++ src/Arlirad.Mediator/Interfaces/IMediator.cs
@@ -0,0 +1,10 @@
+namespace Arlirad.Mediator.Interfaces;
+
+public interface IMediator
+{
+    ValueTask<TResponse> Send<TRequest, TResponse>(TRequest request, CancellationToken ct)
+        where TRequest : IRequest<TResponse>;
+
+    ValueTask Send<TNotification>(TNotification request, CancellationToken ct) where TNotification : INotification;
+    ValueTask FlushHandler(Type handler);
+}
