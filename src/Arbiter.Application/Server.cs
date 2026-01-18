08944c79c6ce001bce465ef2b6d886936391c0cc
Arlirad <arlirad@nodivia.pl>
2026-01-18 13:15:03 +0100

    Refactor Server.Run
    Remove unneccesary Func from Server.cs.

--- src/Arbiter.Application/Server.cs
+++ src/Arbiter.Application/Server.cs
@@ -14,22 +14,24 @@ internal class Server(
         await configManager.CreateDirectories();
         await configManager.InitialConfigure();
 
-        var tasks = acceptors.Select<IAcceptor, Func<Task>>(acceptor => async Task () =>
-        {
-            try
-            {
-                while (!ct.IsCancellationRequested)
-                {
-                    var transaction = await acceptor.Accept(ct);
-                    _ = handler.Handle(transaction);
-                }
-            }
-            catch (OperationCanceledException)
-            {
-                // ignored
-            }
-        });
+        var tasks = acceptors.Select<IAcceptor, Task>(acceptor => Accept(acceptor, ct));
 
-        await Task.WhenAny(tasks.Select(t => t()));
+        await Task.WhenAny(tasks);
+    }
+
+    private async Task Accept(IAcceptor acceptor, CancellationToken ct)
+    {
+        try
+        {
+            while (!ct.IsCancellationRequested)
+            {
+                var transaction = await acceptor.Accept(ct);
+                _ = handler.Handle(transaction);
+            }
+        }
+        catch (OperationCanceledException)
+        {
+            // ignored
+        }
     }
 }
